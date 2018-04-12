055846bf04df9ca98b24ebbf3488ba6bccc166fc
Craig Weber <crgwbr@gmail.com>
2018-04-12 10:32:12 -0400

    Fix tests broken by disapperance of http://www.webservicex.net/uszip.asmx?WSDL

--- src/soap/tests/test_soap.py
+++ src/soap/tests/test_soap.py
@@ -5,28 +5,16 @@ import soap
 
 class SOAPTest(TestCase):
     def test_basic_soap_method_call(self):
-        wsdl = 'http://www.webservicex.net/uszip.asmx?WSDL'
+        wsdl = 'http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL'
         client = soap.get_client(wsdl, 'LOCATIONS')
-        client.set_options(port='USZipSoap')
-
-        resp = client.service.GetInfoByZIP('10305')
-        self.assertEquals(resp.NewDataSet.Table.CITY, 'Staten Island')
-        self.assertEquals(resp.NewDataSet.Table.STATE, 'NY')
+        client.set_options(port='NumberConversionSoap')
+        resp = client.service.NumberToWords('42')
+        self.assertEquals(str(resp).strip(), 'forty two')
 
 
 class SOAPProxyTest(TestCase):
     def test_soap_with_proxy(self):
         """ Set sandbox/settings to use """
         if settings.PROXY_URL:
-            wsdl = 'http://www.webservicex.net/uszip.asmx?WSDL'
+            wsdl = 'http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL'
             soap.get_client(wsdl, 'LOCATIONS')
-
-    def test_private_soap_with_proxy(self):
-        """ Intended for local and private use only
-        Set SOAP_PROXY_URL in sandbox/settings too.
-        """
-        private_ip = ''
-        if not private_ip:
-            return
-        wsdl = 'http://{}/TAXCCH/Service3.5.svc?singleWsdl'.format(private_ip)
-        soap.get_client(wsdl, 'CCH')
